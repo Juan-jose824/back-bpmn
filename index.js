64cda451af21c85db2211cd721ef9951e5980592
Juan-jose824 <juanjoserivera1928@gmail.com>
2026-02-13 10:25:52 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const pool = require('./db');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Servidor BPMN funcionando'));
+
+app.listen(3000, () => console.log('Servidor en http://localhost:3000'));
